216efdc51a1527211bfa203c94d4195589fd8acd
Derrick Obeng <derickobeng023@gmail.com>
2025-08-17 20:25:22 +0000

    checker fixed

--- alx_backend_graphql_crm/graphql_crm/seed_db.py
+++
@@ -1,5 +0,0 @@
-from crm.models import Customer, Product
-
-Customer.objects.create(name="Test User", email="test@example.com", phone="+1234567890")
-Product.objects.create(name="Phone", price=500, stock=5)
-Product.objects.create(name="Tablet", price=800, stock=2)
--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -19,10 +17,15 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="orders")
     products = models.ManyToManyField(Product)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     order_date = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        # Automatically calculate total amount
+        self.total_amount = sum(product.price for product in self.products.all())
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"Order #{self.id} - {self.customer.name}"
+        return f"Order {self.id} for {self.customer.name}"
